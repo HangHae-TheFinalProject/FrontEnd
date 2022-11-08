00a0f82078813de5fa06b3861dfc20de8f9ad30e
hanghea99 <shj3229@naver.com>
2022-11-08 16:19:51 +0900

    신혜정: <feature> signin 기능 작업중

--- src/components/formSignIn/FormSignIn.jsx
+++ src/components/formSignIn/FormSignIn.jsx
@@ -1,6 +1,48 @@
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
+
 import { DivSignIn } from './style';
 
+import useInput from '../../hooks/useInput';
+import axios from 'axios';
+
 export default function FormSignIn() {
+  const [cookie, setCookie, removeCookie] = useCookies();
+
+  const [email, setEmail] = useInput('');
+  const [password, setPassword] = useInput('');
+
+  const nav = useNavigate();
+
+  const signUpClick = () => {
+    nav('/SignUp');
+  };
+
+  const loginOnClickHandler = () => {
+    axios
+      .post('https://haetae.shop/lier/login', {
+        email: email,
+        password: password,
+      })
+      .then((res) => {
+        if (res.data.success) {
+          console.log('요깅', res.data.datd.email);
+          setCookie('token', res.request.getResponseHeader('authorization'), {
+            path: '/',
+          });
+          setCookie(
+            'refreshtoken',
+            res.request.getResponseHeader('refresh-token'),
+            { path: '/' }
+          );
+          sessionStorage.setItem('email', res.data.data.email);
+        } else {
+          alert(res.data.error);
+        }
+      });
+  };
+  axios.post();
+
   return (
     <>
       <DivSignIn>
@@ -9,20 +51,28 @@ export default function FormSignIn() {
         </div>
 
         <div className="divInpuuBox">
-          <input placeholder="  이메일을 입력해주세요."></input>
+          <input
+            type="text"
+            placeholder="  이메일을 입력해주세요."
+            onChange={setEmail}
+          ></input>
           <p>이메일이 틀렸습니다.</p>
-          <input placeholder="  비밀번호를 입력해주세요."></input>
+          <input
+            type="text"
+            placeholder="  비밀번호를 입력해주세요."
+            onChange={setPassword}
+          ></input>
           <p>비밀번호가 틀렸습니다.</p>
         </div>
 
         <div className="divButtonBox">
-          <button>로그인</button>
+          <button onClick={loginOnClickHandler}>로그인</button>
           <button>구글로그인</button>
         </div>
 
         <div className="divSignUpBox">
           <p>아직 라이어게임 참가자가 아니신가요?</p>
-          <p>회원가입</p>
+          <p onClick={signUpClick}>회원가입</p>
         </div>
       </DivSignIn>
     </>
