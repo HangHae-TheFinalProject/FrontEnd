4512384f5de0d190deaa41a22c03d1b396206203
Kim Minjoo <roses16.dev@gmail.com>
2022-11-11 01:07:53 +0900

    김민주: <test> open vidu test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import Router from './shared/Router';
+import VideoRoomComponent from 'openvidu-react/dist/components/VideoRoomComponent';
+import registerServiceWorker from './registerServiceWorker';
 
 function App() {
   return (
   <>
-    <Router />
+  <VideoRoomComponent openviduServerUrl='https://cheiks.shop'/>
+    {/* <Router /> */}
   </>
   );
 }
 
 export default App;
+registerServiceWorker();
