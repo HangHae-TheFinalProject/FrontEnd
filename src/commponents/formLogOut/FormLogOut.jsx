42909cd93690ad135f27b734693def8f7430eb29
hanghea99 <shj3229@naver.com>
2022-11-12 16:06:51 +0900

    신혜정 : <feature> logou 기능 구현 완료

--- src/commponents/formLogOut/FormLogOut.jsx
+++ src/commponents/formLogOut/FormLogOut.jsx
@@ -1,11 +1,14 @@
 import axios from 'axios';
 import { Cookies, useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
 import instance from '../../shared/Request';
 
 export default function FormLogOut() {
+  const nav = useNavigate();
   const logOutClickHandler = async () => {
     try {
       await instance.post('https://haetae.shop/lier/logout');
+      nav('/signin');
     } catch (error) {
       console.log(error);
     }
