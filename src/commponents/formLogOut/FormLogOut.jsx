7fb12ab466b8646d6dac09081718712d0763c096
hanghea99 <shj3229@naver.com>
2022-11-12 09:09:26 +0900

    로그아웃 기능 구현중

--- src/commponents/formLogOut/FormLogOut.jsx
+++ src/commponents/formLogOut/FormLogOut.jsx
@@ -1,15 +1,27 @@
 import axios from 'axios';
+import { Cookies, useCookies } from 'react-cookie';
 
 export default function FormLogOut() {
-  const logOutClickHandler = (props) => {
-    axios.get('https://haetae.shop/lier/logout').then((res) => {
-      if (res.data) {
-        console.log(res);
-        props.history.push('/login');
-      } else {
-        alert('로그아웃 실패');
-      }
-    });
+  const [cookies, setCoocie] = useCookies('');
+  //const authorization = Cookies.getcookie('Authorization');
+  //const refresh_token = sessionStorage.getItem('Refresh-Token');
+  console.log();
+
+  const getCookie = () => {
+    return cookies.get('Authorization');
+  };
+  const logOutClickHandler = (res) => {
+    axios
+      .get('https://haetae.shop/lier/logout', {
+        authorization: `Bearer ${getCookie('is_login')}`,
+      })
+      .then((res) => {
+        if (res.data) {
+          console.log(res);
+        } else {
+          alert('로그아웃 실패');
+        }
+      });
   };
 
   return (
