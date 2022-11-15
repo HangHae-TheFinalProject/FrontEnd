76efa7eded9451dea3a48a516faf5cba14b7e083
hanghea99 <shj3229@naver.com>
2022-11-15 20:55:28 +0900

    신혜정 : <fix> 로그인 토큰 명 수정

--- src/components/formSignIn/FormSignIn.jsx
+++ src/components/formSignIn/FormSignIn.jsx
@@ -28,9 +28,12 @@ export default function FormSignIn() {
       .then((res) => {
         if (res.data.statusCode === '0') {
           console.log('요깅', res);
-          setCookie('token', res.request.getResponseHeader('authorization'));
           setCookie(
-            'refreshtoken',
+            'access_token',
+            res.request.getResponseHeader('authorization')
+          );
+          setCookie(
+            'refresh_token',
             res.request.getResponseHeader('refresh-token')
           );
           sessionStorage.setItem('email', res.data.data.email);
@@ -38,7 +41,6 @@ export default function FormSignIn() {
           nav('/');
         } else {
           console.log(res.data.error);
-          alert(res.data.error);
         }
       });
   };
@@ -58,7 +60,7 @@ export default function FormSignIn() {
           ></input>
 
           <input
-            type="text"
+            type="password"
             placeholder="  비밀번호를 입력해주세요."
             onChange={setPassword}
           ></input>
