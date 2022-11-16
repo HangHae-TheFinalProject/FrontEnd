9bc103343c35e13626edf465ba269c0b068125c7
hanghea99 <shj3229@naver.com>
2022-11-17 04:39:22 +0900

    신혜정 : <css> 회원가입 스타일 수정

--- src/components/formSignUp/FormSignUp.jsx
+++ src/components/formSignUp/FormSignUp.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 import useInput from '../../hooks/useInput';
-import { DivSignUp } from './style';
+import './style.scss';
 
 export default function FormSignUp() {
   const [name, setName] = useInput('');
@@ -40,50 +40,57 @@ export default function FormSignUp() {
   };
 
   return (
-    <DivSignUp>
-      <div className="divTitleBox">
-        <h2>회원가입</h2>
-      </div>
+    <>
+      <div className="divSignInBox">
+        <div className="divTitleBox">
+          <h2>회원가입</h2>
+        </div>
 
-      <div className="divInputBox">
-        <label> NAME </label>
-        <input
-          type="text"
-          onChange={setName}
-          placeholder="이름을 입력하세요"
-        ></input>
-        <label> EMAIL </label>
-        <input
-          type="text"
-          onChange={setMail}
-          placeholder="메일을 입력하세요"
-        ></input>
-      </div>
-
-      <div className="divInputPassword">
-        <label> PASSWORD </label>
-        <input
-          type="password"
-          name="password"
-          placeholder="비밀번호를 입력하세요"
-          onChange={setPassword}
-        ></input>
-        <label> PASSWORD </label>
-        <input
-          type="password"
-          placeholder="비밀번호를 입력하세요"
-          onChange={setPasswordConfirm}
-        ></input>
-        {password !== passwordConfirm ? (
+        <div className="divInputBox">
+          <input
+            type="text"
+            onChange={setName}
+            placeholder="이름을 입력하세요"
+          ></input>
           <p>비밀번호가 일치하지 않습니다</p>
-        ) : null}
-      </div>
 
-      <div className="divSignUpBox">
-        <button onClick={joinOnClickHandler}>회원가입</button>
-        <p>기존 라이어게임 참가자이신가요?</p>
-        <p onClick={loginClick}>로그인</p>
+          <input
+            type="text"
+            onChange={setMail}
+            placeholder="메일을 입력하세요"
+          ></input>
+          <p>비밀번호가 일치하지 않습니다</p>
+        </div>
+
+        <div className="divInputPassword">
+          <input
+            type="password"
+            name="password"
+            placeholder="비밀번호를 입력하세요"
+            onChange={setPassword}
+          ></input>
+          <p>비밀번호가 일치하지 않습니다</p>
+
+          <input
+            type="password"
+            placeholder="비밀번호를 입력하세요"
+            onChange={setPasswordConfirm}
+          ></input>
+          {password !== passwordConfirm ? (
+            <p>비밀번호가 일치하지 않습니다</p>
+          ) : null}
+        </div>
+
+        <div className="divSignUpBox">
+          <button onClick={joinOnClickHandler}>회원가입</button>
+          <div className="divLoginText">
+            <p className="loginText">기존 라이어게임 참가자이신가요?</p>
+            <p className="loginBtn" onClick={loginClick}>
+              로그인
+            </p>
+          </div>
+        </div>
       </div>
-    </DivSignUp>
+    </>
   );
 }
