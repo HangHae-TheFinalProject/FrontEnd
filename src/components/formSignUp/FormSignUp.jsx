c0d97b8e977f5c603e2e83dadcb11c65b7b4854f
hanghea99 <shj3229@naver.com>
2022-11-17 21:14:41 +0900

    신혜정 : <css> css signup 수정

--- src/components/formSignUp/FormSignUp.jsx
+++ src/components/formSignUp/FormSignUp.jsx
@@ -41,53 +41,55 @@ export default function FormSignUp() {
 
   return (
     <>
-      <div className="divSignInBox">
-        <div className="divTitleBox">
-          <h2>회원가입</h2>
-        </div>
+      <div className="signUpBox">
+        <div className="divSignInBox">
+          <div className="divTitleBox">
+            <h2>회원가입</h2>
+          </div>
 
-        <div className="divInputBox">
-          <input
-            type="text"
-            onChange={setName}
-            placeholder="이름을 입력하세요"
-          ></input>
-          <p>비밀번호가 일치하지 않습니다</p>
-
-          <input
-            type="text"
-            onChange={setMail}
-            placeholder="메일을 입력하세요"
-          ></input>
-          <p>비밀번호가 일치하지 않습니다</p>
-        </div>
-
-        <div className="divInputPassword">
-          <input
-            type="password"
-            name="password"
-            placeholder="비밀번호를 입력하세요"
-            onChange={setPassword}
-          ></input>
-          <p>비밀번호가 일치하지 않습니다</p>
-
-          <input
-            type="password"
-            placeholder="비밀번호를 입력하세요"
-            onChange={setPasswordConfirm}
-          ></input>
-          {password !== passwordConfirm ? (
+          <div className="divInputBox">
+            <input
+              type="text"
+              onChange={setName}
+              placeholder="이름을 입력하세요"
+            ></input>
             <p>비밀번호가 일치하지 않습니다</p>
-          ) : null}
-        </div>
 
-        <div className="divSignUpBox">
-          <button onClick={joinOnClickHandler}>회원가입</button>
-          <div className="divLoginText">
-            <p className="loginText">기존 라이어게임 참가자이신가요?</p>
-            <p className="loginBtn" onClick={loginClick}>
-              로그인
-            </p>
+            <input
+              type="text"
+              onChange={setMail}
+              placeholder="메일을 입력하세요"
+            ></input>
+            <p>비밀번호가 일치하지 않습니다</p>
+          </div>
+
+          <div className="divInputPassword">
+            <input
+              type="password"
+              name="password"
+              placeholder="비밀번호를 입력하세요"
+              onChange={setPassword}
+            ></input>
+            <p>비밀번호가 일치하지 않습니다</p>
+
+            <input
+              type="password"
+              placeholder="비밀번호를 입력하세요"
+              onChange={setPasswordConfirm}
+            ></input>
+            {password !== passwordConfirm ? (
+              <p>비밀번호가 일치하지 않습니다</p>
+            ) : null}
+          </div>
+
+          <div className="divSignUpBox">
+            <button onClick={joinOnClickHandler}>회원가입</button>
+            <div className="divLoginText">
+              <p className="loginText">기존 라이어게임 참가자이신가요?</p>
+              <p className="loginBtn" onClick={loginClick}>
+                로그인
+              </p>
+            </div>
           </div>
         </div>
       </div>
