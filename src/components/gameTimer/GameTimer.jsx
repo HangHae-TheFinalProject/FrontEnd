a78373cbf8b77671a4aa5e300a018b75d00a6d6e
Kim Minjoo <roses16.dev@gmail.com>
2022-11-20 02:10:54 +0900

    김민주: <feature> GameTimer
    - GameRoom에서 setTimer 함수를 통해 시간 지정 가능

--- src/components/gameTimer/GameTimer.jsx
+++ src/components/gameTimer/GameTimer.jsx
@@ -1,12 +1,33 @@
 import './style.scss';
 import timerFrame from '../../images/svg/timerFrame.svg';
+import { useState, useEffect } from 'react';
 
-function GameTimer() {
+function GameTimer({ sec, setSec }) {
 
+  const [time, setTime] = useState();
+
+  useEffect(() => {
+    setTime(sec);
+    const intervalId = setInterval(() => {
+      setTime((t) => t - 1)
+    }, 1000);
+
+    const timeoutId = setTimeout(() => {
+      clearInterval(intervalId)
+      setTime(-1);
+      setSec(-1);
+    }, (sec + 1) * 1000)
+
+    return () => {
+      return (clearInterval(intervalId),
+        clearTimeout(timeoutId)
+      );
+    }
+  }, [sec])
 
   return <div className='timerBox'>
-    <img src={timerFrame} className='timerFrame'/>
-    <div className='timerText fontSemiBold'>15</div>
+    <img src={timerFrame} className='timerFrame' />
+    <div className={`timerText fontSemiBold ${time === 0 ? 'fontred' : ''}`}>{time >= 0 ? time : ''}</div>
   </div>
 }
 
