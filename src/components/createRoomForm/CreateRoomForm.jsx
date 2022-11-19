df89ea00f89c06d919ee2ed19a2928c11a6762e1
Kim Minjoo <roses16.dev@gmail.com>
2022-11-20 01:56:47 +0900

    김민주: <fix> 룸 생성자는 redux 내 Room 정보가 공란인 문제 수리

--- src/components/createRoomForm/CreateRoomForm.jsx
+++ src/components/createRoomForm/CreateRoomForm.jsx
@@ -1,11 +1,14 @@
 import useInput from '../../hooks/useInput';
 import instance from '../../shared/Request';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { setRoom } from '../../redux/modules/roomsSlice';
 
 import './style.scss';
 
 // need to : 방 만들고 입장하는 기능
 function CreateRoomForm() {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const DEFAULT_GAMEMODE = '1';
   const [title, titleHandler] = useInput('기본 방 제목');
@@ -14,14 +17,13 @@ function CreateRoomForm() {
   
   const createRoomHandler = async () => {
     try{
-      console.log(title, password, gamemode)
       const { data } = await instance.post('/lier/room', {
         roomName: title,
         roomPassword: password,
         mode: gamemode*1
       })
+      dispatch(setRoom(data.data))
       navigate(`/gameroom/${data.data.roomId}`);
-      console.log(data)
     } catch (error) {
       alert('방 생성에 실패했습니다.');
       navigate('/');
