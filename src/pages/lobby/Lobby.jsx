6ca81531145af079f4b100f27c9cd1350e34a694
hanghea99 <shj3229@naver.com>
2022-11-22 11:04:53 +0900

    로비스타일 작업중

--- src/pages/lobby/Lobby.jsx
+++ src/pages/lobby/Lobby.jsx
@@ -4,26 +4,45 @@ import { useNavigate } from 'react-router-dom';
 import Modal from '../../elements/modal/Modal';
 import RoomList from '../../components/roomList/RoomList';
 import CreateRoomForm from '../../components/createRoomForm/CreateRoomForm';
+import lobbyBackGround from '../../images/png/lobbyBackGround.png';
 
-import './style.scss'
+import './style.scss';
 
 function Lobby() {
   const navigate = useNavigate();
   const [openModal, setOpenModal] = useState(false);
 
-  useEffect(() => {
-    
-  }, [])
+  useEffect(() => {}, []);
 
-  return <div className='pagesection'>
-    <div>
-      <input type='button' value='방만들기' onClick={()=>setOpenModal(true)}/>
-      <input type='button' value='커뮤니티' onClick={()=>navigate('/social/list')}/>
-      <input type='button' value='마이페이지' onClick={()=>navigate('/mypage')}/>
+  return (
+    <div className="pagesection">
+      <img src={lobbyBackGround} className="background" />
+      <div>
+        <input
+          type="button"
+          value="방만들기"
+          onClick={() => setOpenModal(true)}
+        />
+        <input
+          type="button"
+          value="커뮤니티"
+          onClick={() => navigate('/social/list')}
+        />
+        <input
+          type="button"
+          value="마이페이지"
+          onClick={() => navigate('/mypage')}
+        />
+      </div>
+      <RoomList />
+      {openModal && (
+        <Modal
+          onClose={() => setOpenModal(false)}
+          content={<CreateRoomForm />}
+        />
+      )}
     </div>
-    <RoomList />
-    {openModal && <Modal onClose={()=>setOpenModal(false)} content={<CreateRoomForm />} />}
-  </div>
+  );
 }
 
 export default Lobby;
