1ee7feaf05d2797dca6dec50d735b085595e6ed0
Kim Minjoo <roses16.dev@gmail.com>
2022-11-23 22:50:41 +0900

    김민주: <fix> login 직후 header에 token이 담기지 않는 오류 수정 #69

--- src/shared/Request.js
+++ src/shared/Request.js
@@ -1,23 +1,19 @@
 import axios from 'axios';
-import { Cookies } from 'react-cookie';
-
-const access_token = new Cookies().get('access_token');
-const refresh_token = new Cookies().get('refresh_token');
-
-// axios.defaults.headers.common['authorization'] = `${access_token}`;
-// axios.defaults.headers.common['refresh-token'] = `${refresh_token}`;
 
 const instance = axios.create({
   baseURL: 'https://haetae.shop',
   headers: {
-    authorization: access_token,
-    'refresh-token': refresh_token,
     'Content-Type': 'application/json',
   },
 });
 
-export const chat = axios.create({
-  headers: {},
-});
-
 export default instance;
+
+export const setClientHeaders = (token) => {
+  console.log('interceptor');
+  instance.interceptors.request.use(function (config) {
+    config.headers['authorization'] = token.access;
+    config.headers['refresh-token'] = token.refresh;
+    return config;
+  });
+};
