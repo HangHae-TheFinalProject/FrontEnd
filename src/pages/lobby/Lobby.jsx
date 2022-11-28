6a9f943bc2cdd98a7b1dd52c3bf2739716aa7d8e
Kim Minjoo <roses16.dev@gmail.com>
2022-11-29 00:07:13 +0900

    김민주: <feature> 게임룸 입장 전 디바이스 체크 Lobby > GameRoom 진입 직후로 변경
    - Lobby에서는 실제 카메라 또는 마이크를 사용하지 않음에도 입장 제한을 위해 불필요한 디바이스 체크가 진행되어 이를 GameRoom 입장 직후로 변경

--- src/pages/lobby/Lobby.jsx
+++ src/pages/lobby/Lobby.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 import Modal from '../../elements/modal/Modal';
 import RoomList from '../../components/roomList/RoomList';
@@ -8,36 +9,25 @@ import roomMakerBtn from '../../images/png/roomMakerBtn.png';
 import LobbyHeader from '../../components/robbyHeader/RobbyHeader';
 
 import { setIsCamera, setIsCantGetDevice } from '../../redux/modules/gameSlice'
-import { useDispatch } from 'react-redux';
 import './style.scss';
+
+// import BGM from '../../audio/lobbyBGM.mp3';
 
 function Lobby() {
   const [openModal, setOpenModal] = useState(false);
   const dispatch = useDispatch();
+  const BGM = new Audio('../../audio/lobbyBGM.mp3');
   let isCantGetDevice = false;
 
-  navigator.mediaDevices.getUserMedia({ audio: true, video: true })
-    .then(res => {
-      console.log(res.getAudioTracks());
-      console.log(res.getVideoTracks());
-
-      dispatch(setIsCamera( res.getVideoTracks()[0] || res.getAudioTracks()[0] ? true : false));
-      isCantGetDevice = false;
-    })
-    .catch(err => {
-      alert('다른 브라우저에서 마이크 또는 비디오를 사용중입니다. 게임방 입장이 어려울 수 있습니다.');
-      isCantGetDevice = true;
-      dispatch(setIsCantGetDevice(isCantGetDevice));
-    })
-
-
   const craeteRoomHandler = () => {
-    if(isCantGetDevice) {
-      alert('다른 브라우저에서 마이크 또는 비디오를 사용중입니다. 게임방 입장이 어려울 수 있습니다.');
-      return;
-    }
+
     setOpenModal(true)
   }
+
+  useEffect(() => {
+    console.log('useEffect');
+
+  }, [])
 
   return (
     <div className="pagesection">
