5128ae0de32aec1b786171b57257349bd77bcdc6
keepfall <kurney@naver.com>
2022-12-03 16:33:54 +0900

    정건희: <style> Lobby 배경화면, 헤더 css 수정
    로비 배경화면 css 정리 및 추가 / object-fit: cover
    로비, 소셜 헤더바 텍스트 정렬 css 정리 및 수정
    .gitignore 내 .env 중복 코드 삭제

--- src/components/lobbyHeader/LobbyHeader.jsx
+++ src/components/lobbyHeader/LobbyHeader.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Modal from '../../elements/modal/Modal';
+
 import headerArrow from '../../images/png/headerArrow.png';
 import MyPageForm from '../myPage/myPageForm/MyPageForm';
 
@@ -12,21 +13,26 @@ function LobbyHeader() {
 
   return (
     <>
-      <div className="headerbackground fontLightBold">
-        <header className="headerBtnBox">
-          <div className="communityBox" onClick={() => navigate('/social')}>
+      <div className="lobbyHeaderBackground fontLightBold">
+        <header className="lobbyHeader">
+          <h1>로비</h1>
+        </header>
+        <div className="lobbyHeaderCommunityBox">
+          <div
+            className="lobbyHeaderCommunityBtn"
+            onClick={() => navigate('/social')}
+          >
             <img src={headerArrow} alt="headerArrow" />
             <span>커뮤니티</span>
           </div>
-          <h1>로비</h1>
-          <span onClick={() => setOpenModal(true)}>마이페이지</span>
           {openModal && (
             <Modal
               onClose={() => setOpenModal(false)}
               content={<MyPageForm />}
             />
           )}
-        </header>
+          <span onClick={() => setOpenModal(true)}>마이페이지</span>
+        </div>
       </div>
     </>
   );
