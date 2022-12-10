3b13eee9219aa425f50d24120209abb46e910fe7
Kim Minjoo <roses16.dev@gmail.com>
2022-12-10 10:11:49 +0900

    김민주: <fix> console log 제거

--- src/pages/gameroom/GameRoom.jsx
+++ src/pages/gameroom/GameRoom.jsx
@@ -149,7 +149,7 @@ function GameRoom() {
   const subscribe = () => {
     client.current.subscribe(`/sub/gameroom/${id}`, ({ body }) => {
       const data = JSON.parse(body);
-      console.log(data);
+      // console.log(data);
       switch (data.type) {
         case 'JOIN':
           setMemberCount(data.content.memberCnt);
